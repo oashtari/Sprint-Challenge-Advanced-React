dd88532fbd1dd7f7e44082754cde045927bf296b
oashtari <oashtari@gmail.com>
2020-01-26 19:32:31 -0800

    finished data rendering to page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PlayerCard from './Components/WWCplayersCard';
+
+class App extends React.Component {
+
+
+  constructor() {
+    super();
+    this.state = {
+      players: []
+      // name: '',
+      // country: '',
+      // searches: '',
+      // id: ''
+
+    }
+  }
+
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:5000/api/players`)
+      .then(res => {
+
+        this.setState({ ...this.state, players: res.data })
+
+      })
+      .catch(err => console.log(err))
+  }
+
+
+
+
+  render() {
+    return (
+      <div className="App" >
+        <div className='wwc_players'>
+          {/* <FilterPlayers props={this.state.playersList} /> */}
+          <h1> WWC Players:</h1 >
+          {this.state.players.map(player => (
+            < PlayerCard player={player} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ client/src/Components/WWCplayersCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card, CardTitle, CardBody, CardText } from 'reactstrap';
+
+const PlayerCard = props => {
+    return (
+        <div style={{ margin: '20px' }}>
+            <Card style={{ backgroundColor: '#b3ffff', width: '300px' }} className='allPlayers'>
+                <CardText className='test'>Name: {props.player.name}</CardText>
+                <CardText className='test'>Country: {props.player.country}</CardText>
+                <CardText className='test'>Search rank: {props.player.searches}</CardText>
+            </Card>
+        </div>
+    )
+
+}
+
+export default PlayerCard;
